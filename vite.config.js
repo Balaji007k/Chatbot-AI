a3bf53e7945702134c8e508f17c5e21dd8763a6d
Balaji007k <k446521@gmail.com>
2025-07-12 17:04:32 +0530

    small changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ghPages from 'vite-plugin-gh-pages';
 
 const repoName = 'Chatbot-AI';
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),react(),ghPages()
+    tailwindcss(),react(),
   ],
   base: `/${repoName}/`,
 })
